266bd57e81c5308df78d7adc6f6b1bce2a649bf8
netzego <github@netzego.de>
2023-04-27 14:47:35 +0200

    wip: rename main.bash to ss.bash

--- ss.bash
+++ ss.bash
@@ -7,9 +7,19 @@ readonly scriptdir="$(dirname "$(realpath "${BASH_ARGV0}")")"
 source "${scriptdir}/include/vars.in"
 source "${scriptdir}/include/func.in"
 
+print_vars() {
+	echo "${FUNCNAME[0]}"
+	echo "scriptdir: ${scriptdir}"
+	echo "scriptname: ${scriptname}"
+	echo "pwd: ${pwd}"
+	echo "lockfile: ${lockfile}"
+	echo "configfile: ${configfile}"
+}
+
 main() {
 	check_root
 	check_lockfile
+	print_vars
 }
 
 main
