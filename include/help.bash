67640146b57dbe3de31ca525504780a5df1c4c0f
netzego <github@netzego.de>
2023-05-01 06:57:43 +0200

    wip: ref help.bash in extra file

---
+++ include/help.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# shellcheck disable=SC2154
+help() {
+	echo "${BASH_ARGV0} (v$version)"
+	echo ""
+	echo "-h, --help      print this message"
+	echo "-v, --version   print the version and exit"
+	echo "-d, --debug     enable debug mode"
+	echo "-v, --verbose   enable verbose mode"
+}
