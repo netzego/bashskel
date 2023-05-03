dee8964a7dc0a0e5b40f7d8cf58ebb2875f09352
netzego <github@netzego.de>
2023-05-03 22:30:56 +0200

    wip: src: autof & fix shellcheck

--- include/parse_args.bash
+++ include/parse_args.bash
@@ -3,50 +3,52 @@
 # DESC: wraps getopt to parse cli args
 # ARGS: $@: arguments from the command line
 # NOTE: sets globa vars. such as $action, $pargs, etc.
+# shellcheck disable=SC2154,SC2034
 parse_args() {
-	local name="${f_red}***${a_norm} ${scriptname}"
-	temp=$(getopt -o "hvdm:" -l "help,version,debug,mode:" -n "${name}" -- "$@")
+    local name="${f_red}***${a_norm} ${scriptname}"
+    temp=$(getopt -o "hvdm:" -l "help,version,debug,mode:" -n "${name}" -- "$@")
 
-	# `$temp` MUST not be a local variable! otherwise this will NOT work
-	if [ $? -ne 0 ]; then
-		exit 2
-	fi
+    # `$temp` MUST not be a local variable! otherwise this will NOT work
+    # shellcheck disable=SC2181
+    if [ $? -ne 0 ]; then
+        exit 2
+    fi
 
-	# `$temp` MUST be quoted!
-	eval set -- "$temp"
-	unset temp
+    # `$temp` MUST be quoted!
+    eval set -- "$temp"
+    unset temp
 
-	while true; do
-		case "$1" in
-		"-h" | "--help")
-			readonly action="help"
-			shift
-			continue
-			;;
-		"-v" | "--version")
-			readonly action="version"
-			shift
-			continue
-			;;
-		"-d" | "--debug")
-			readonly debug=true
-			shift
-			continue
-			;;
-		"-m" | "--mode")
-			readonly mode="${2}"
-			shift 2
-			continue
-			;;
-		"--")
-			shift
-			break
-			;;
-			# *)
-			#     die "err: parse_args"
-			# ;;
-		esac
-	done
+    while true; do
+        case "$1" in
+        "-h" | "--help")
+            readonly action="help"
+            shift
+            continue
+            ;;
+        "-v" | "--version")
+            readonly action="version"
+            shift
+            continue
+            ;;
+        "-d" | "--debug")
+            readonly debug=true
+            shift
+            continue
+            ;;
+        "-m" | "--mode")
+            readonly mode="${2}"
+            shift 2
+            continue
+            ;;
+        "--")
+            shift
+            break
+            ;;
+            # *)
+            #     die "err: parse_args"
+            # ;;
+        esac
+    done
 
-	readonly pargs="$*"
+    readonly pargs="$*"
 }
