f91bccce5ee6572d7a121316fd3575535decddd6
netzego <github@netzego.de>
2023-05-08 23:55:15 +0200

    src: remove leftover MODE arg_parse logic

--- include/parse_args.bash
+++ include/parse_args.bash
@@ -7,7 +7,7 @@
 # shellcheck disable=SC2154,SC2034
 parse_args() {
     local name="${f_red}***${a_norm} ${SCRIPTNAME}"
-    temp=$(getopt -o "hvdm:" -l "help,verbose,debug,mode:,version" -n "${name}" -- "$@")
+    temp=$(getopt -o "hvd" -l "help,verbose,debug,version" -n "${name}" -- "$@")
 
     # `$temp` MUST not be a local variable! otherwise this will NOT work
     # shellcheck disable=SC2181
