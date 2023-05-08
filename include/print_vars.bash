d16c840133e0c40b83732f278cc5aff57d5bd2e1
netzego <github@netzego.de>
2023-05-08 13:11:02 +0200

    src: refactor: use upper vars

--- include/print_vars.bash
+++ include/print_vars.bash
@@ -4,12 +4,12 @@
 # ARGS: None
 # shellcheck disable=SC2154
 print_vars() {
-    printf "%s %16s: %s\n" "--" "scriptname" "${scriptname}"
-    printf "%s %16s: %s\n" "--" "pwd" "${pwd}"
-    printf "%s %16s: %s\n" "--" "version" "${version}"
-    printf "%s %16s: %s\n" "--" "lockfile" "${lockfile}"
-    printf "%s %16s: %s\n" "--" "configfile" "${configfile}"
-    printf "%s %16s: %s\n" "--" "verbose" "${verbose}"
-    printf "%s %16s: %s\n" "--" "debug" "${debug}"
-    printf "%s %16s: %s\n" "--" "action" "${action}"
+    printf "%s %16s: %s\n" "--" "SCRIPTNAME" "${scriptname}"
+    printf "%s %16s: %s\n" "--" "PWD" "${pwd}"
+    printf "%s %16s: %s\n" "--" "VERSION" "${version}"
+    printf "%s %16s: %s\n" "--" "LOCKFILE" "${lockfile}"
+    printf "%s %16s: %s\n" "--" "CONFIGFILE" "${configfile}"
+    printf "%s %16s: %s\n" "--" "VERBOSE" "${verbose}"
+    printf "%s %16s: %s\n" "--" "DEBUG" "${debug}"
+    printf "%s %16s: %s\n" "--" "ACTION" "${action}"
 }
