3af10c676f324541dde358bb106097b156229383
netzego <github@netzego.de>
2023-05-08 15:07:14 +0200

    src: rename to VERSION_NUMBER

--- include/vars.bash
+++ include/vars.bash
@@ -3,6 +3,6 @@
 
 readonly PWD="$(pwd)"
 readonly SCRIPTNAME="$(basename "${BASH_ARGV0%.*}")"
-readonly VERSION="0"
+readonly VERSION_NUMBER="0"
 readonly LOCKFILE="/tmp/${SCRIPTNAME}.lock"
 readonly CONFIGFILE="${PWD}/${SCRIPTNAME}.config"
