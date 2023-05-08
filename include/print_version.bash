9fe9bee721b755163da75efdddbe645f880a2dae
netzego <github@netzego.de>
2023-05-08 15:21:47 +0200

    src: feat: func print_version

---
+++ include/print_version.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# DESC: Prints the version number and exit
+# ARGS: None
+# shellcheck disable=SC2154
+print_version() {
+    echo "${SCRIPTNAME} v${VERSION_NUMBER}"
+
+    exit 42
+}
