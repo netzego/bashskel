ce213e89a0e13fb2c6aaefbbb3cab2942ab192f0
netzego <github@netzego.de>
2023-05-08 23:25:34 +0200

    src: fix: leftover from upper vars refactor

--- include/print_vars.bash
+++ include/print_vars.bash
@@ -4,12 +4,11 @@
 # ARGS: None
 # shellcheck disable=SC2154
 print_vars() {
-    printf "%s %16s: %s\n" "--" "SCRIPTNAME" "${scriptname}"
-    printf "%s %16s: %s\n" "--" "PWD" "${pwd}"
-    printf "%s %16s: %s\n" "--" "VERSION" "${version}"
-    printf "%s %16s: %s\n" "--" "LOCKFILE" "${lockfile}"
-    printf "%s %16s: %s\n" "--" "CONFIGFILE" "${configfile}"
-    printf "%s %16s: %s\n" "--" "VERBOSE" "${verbose}"
-    printf "%s %16s: %s\n" "--" "DEBUG" "${debug}"
-    printf "%s %16s: %s\n" "--" "ACTION" "${action}"
+    printf "%s %16s: %s\n" "--" "SCRIPTNAME" "${SCRIPTNAME}"
+    printf "%s %16s: %s\n" "--" "PWD" "${PWD}"
+    printf "%s %16s: %s\n" "--" "VERSION_NUMBER " "${VERSION_NUMBER}"
+    printf "%s %16s: %s\n" "--" "LOCKFILE" "${LOCKFILE}"
+    printf "%s %16s: %s\n" "--" "CONFIGFILE" "${CONFIGFILE}"
+    printf "%s %16s: %s\n" "--" "VERBOSE" "${VERBOSE}"
+    printf "%s %16s: %s\n" "--" "DEBUG" "${DEBUG}"
 }
