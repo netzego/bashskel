943801133b1bbdb2281db8e7c3f8bdd7f0baf637
netzego <github@netzego.de>
2023-05-08 13:08:18 +0200

    src: refactor: use upper vars; autoformat

--- include/funcs.bash
+++ include/funcs.bash
@@ -4,44 +4,44 @@
 # ARGS: $@: messages to print
 # shellcheck disable=2154
 log() {
-	echo -e "${a_bold}>>>${a_norm}" "$*"
+    echo -e "${a_bold}>>>${a_norm}" "$*"
 }
 
 # DESC: prints $@ to stderr and exit with return value 128
 # ARGS: $@: messages to print
 # shellcheck disable=2154
 die() {
-	echo -e "${f_red}***${a_norm}" "$*" >&2
-	exit 128
+    echo -e "${f_red}***${a_norm}" "$*" >&2
+    exit 128
 }
 
 # DESC: Check if $UID is equal 0
 # ARGS: None
 check_root() {
-	if [ $UID -ne 0 ]; then
-		echo "must run as root" >&2
-		exit 1
-	fi
+    if [ $UID -ne 0 ]; then
+        echo "must run as root" >&2
+        exit 1
+    fi
 }
 
-# DESC: Check if $lockfile exists to prevent running multiple instances
+# DESC: Check if $LOCKFILE exists to prevent running multiple instances
 # ARGS: None
 # shellcheck disable=2154
 check_lockfile() {
-	if [ -e "${lockfile}" ]; then
-		echo "\`${lockfile}' exists" >&2
-		exit 1
-	fi
+    if [ -e "${LOCKFILE}" ]; then
+        echo "\`${LOCKFILE}' exists" >&2
+        exit 1
+    fi
 }
 
-# DESC: Loads $pwd/$scriptname.config file
+# DESC: Loads $PWD/$SCRIPTNAME.config file
 # ARGS: None
 # shellcheck disable=SC2154,SC1090
 load_configfile() {
-	if [ -e "${configfile}" ]; then
-		source "${configfile}"
-	else
-		echo "\`${configfile}' not found" >&2
-		exit 1
-	fi
+    if [ -e "${CONFIGFILE}" ]; then
+        source "${CONFIGFILE}"
+    else
+        echo "\`${CONFIGFILE}' not found" >&2
+        exit 1
+    fi
 }
