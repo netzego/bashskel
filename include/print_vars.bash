8bcdf24b739821cc150d94c3dd03c0436786093b
netzego <github@netzego.de>
2023-05-14 09:55:05 +0200

    add printing of $POS_ARGS

--- include/print_vars.bash
+++ include/print_vars.bash
@@ -11,6 +11,7 @@ print_vars() {
     printf "%s %16s: %s\n" "--" "CONFIGFILE" "${CONFIGFILE}"
     printf "%s %16s: %s\n" "--" "VERBOSE" "${VERBOSE}"
     printf "%s %16s: %s\n" "--" "DEBUG" "${DEBUG}"
+    printf "%s %16s: %s\n" "--" "POS_ARGS" "${POS_ARGS}"
     printf "%s %16s: %s\n" "--" "FOO" "${FOO}"
     printf "%s %16s: %s\n" "--" "BAR" "${BAR}"
 }
