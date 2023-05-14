7d5ac0ef5decb643dc7cffc4475b97a1fe901f7a
netzego <github@netzego.de>
2023-05-14 12:33:29 +0200

    src: add remove_lockfile func

--- include/cleanup.bash
+++ include/cleanup.bash
@@ -2,10 +2,14 @@
 
 cleanup() {
     # remove $LOCKFILE
+# DESC: remove $LOCKFILE if any
+# ARGS: none
+remove_lockfile() {
     if [ -e "${LOCKFILE}" ]; then
         rm -v "${LOCKFILE}"
     fi
 }
+
 # DESC: clean up artifacts on exit signals
 # ARGS: none
 # NOTE: this is meant to call from a trap
