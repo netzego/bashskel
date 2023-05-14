4bbb948cfdca2fa1f436fd4c66b93eeba7d554b7
netzego <github@netzego.de>
2023-05-14 12:55:29 +0200

    src: fix misspelled check_path_prefix func name

--- include/check_args.bash
+++ include/check_args.bash
@@ -3,7 +3,7 @@
 # DESC: check if path prefix exist.
 # ARGS: $1: path to check
 # ECHO: boolean
-check_prefix_path() {
+check_path_prefix() {
     local path="$1"
     local prefix="$(realpath "$(dirname "${path}")")"
 
@@ -12,4 +12,21 @@ check_prefix_path() {
     else
         echo false
     fi
+}
+
+# DESC: exit if $LOGFILE path prefix NOT exists or is NOT writeable
+# NOTE: this must run only after load_configfile and parse_args was called
+check_logfile() {
+    if [ $(check_path_prefix "${LOGFILE}") = true ]; then
+        die 1 ""
+    fi
+
+    if [ ! -w "${LOGFILE}" ]; then
+        die 1 "\`$LOGFILE' is not writeable"
+    fi
+}
+
+# DESC:
+check_args() {
+    check_logfile
 }
