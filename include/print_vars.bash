3aeb0313cd476646a47b837fb8012ce1dd758cc0
netzego <github@netzego.de>
2023-05-15 00:09:58 +0200

    src: add ARGS, ARGS_LENGTH and POS_ARGS_LENGTH

--- include/print_vars.bash
+++ include/print_vars.bash
@@ -4,6 +4,8 @@
 # ARGS: None
 # shellcheck disable=SC2154
 print_vars() {
+    printf "%s %16s: %s\n" "--" "ARGS" "${ARGS[*]}"
+    printf "%s %16s: %s\n" "--" "ARGS_LENGHT" "${#ARGS[@]}"
     printf "%s %16s: %s\n" "--" "SCRIPTNAME" "${SCRIPTNAME}"
     printf "%s %16s: %s\n" "--" "PWD" "${PWD}"
     printf "%s %16s: %s\n" "--" "VERSION_NUMBER " "${VERSION_NUMBER}"
@@ -11,7 +13,8 @@ print_vars() {
     printf "%s %16s: %s\n" "--" "CONFIGFILE" "${CONFIGFILE}"
     printf "%s %16s: %s\n" "--" "VERBOSE" "${VERBOSE}"
     printf "%s %16s: %s\n" "--" "DEBUG" "${DEBUG}"
-    printf "%s %16s: %s\n" "--" "POS_ARGS" "${POS_ARGS}"
+    printf "%s %16s: %s\n" "--" "POS_ARGS" "${POS_ARGS[*]}"
+    printf "%s %16s: %s\n" "--" "POS_ARGS_LENGTH" "${#POS_ARGS[@]}"
     printf "%s %16s: %s\n" "--" "FOO" "${FOO}"
     printf "%s %16s: %s\n" "--" "BAR" "${BAR}"
 }
