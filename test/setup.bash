f815da71f6b28af739ddd418f6dc4ce56225708e
netzego <github@netzego.de>
2023-05-25 11:26:55 +0200

    test: add comment

--- test/setup.bash
+++ test/setup.bash
@@ -3,6 +3,7 @@
 
 # DESC: this functions runs once for every bat file
 # ARGS: none
+# NOTE: overwrite $0 and $BASH_ARGV0. might not smart or it's a bug in bats?
 setup() {
     readonly SCRIPTDIR="$(realpath "$(dirname "${BATS_TEST_FILENAME}")")/.."
     readonly BASH_ARGV0="bashskel.bash"
