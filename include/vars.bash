d32896bd8d9be4fa0a90585d5457c96619f1ad5e
netzego <github@netzego.de>
2023-11-14 12:07:08 +0100

    src: remove `$PWD` variable; use `$SCRIPTDIR`

--- include/vars.bash
+++ include/vars.bash
@@ -1,8 +1,7 @@
 #!/bin/bash
 # shellcheck disable=SC2034
 
-declare -rg PWD="$(pwd)"
 declare -rg SCRIPTNAME="$(basename "${BASH_ARGV0%.*}")"
 declare -rg VERSION_NUMBER="0"
 declare -rg LOCKFILE="/tmp/${SCRIPTNAME}.lock"
-declare -rg CONFIGFILE="${PWD}/${SCRIPTNAME}.config"
+declare -rg CONFIGFILE="${SCRIPTDIR}/${SCRIPTNAME}.config"
